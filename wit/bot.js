60fd6f4130e8d18477f7c14a853a330bf45e6e9d
Bearguy <stephen@peterkins.ca>
2017-06-13 20:31:35 -0400

    Commented out then

--- wit/bot.js
+++ wit/bot.js
@@ -23,8 +23,8 @@ const actions = {
       // Yay, we found our recipient!
       // Let's forward our bot response to her.
       // We return a promise to let our bot know when we're done sending
-      return sendHelper.sendTextMessage(recipientId, text)
-      .then(() => null)
+      return sendHelper.sendTextMessage(recipientId, text);
+      /*.then(() => null)
       .catch((err) => {
         console.error(
           'Oops! An error occurred while forwarding the response to',
@@ -32,7 +32,7 @@ const actions = {
           ':',
           err.stack || err
         );
-      });
+      });*/
     } else {
       console.error('Oops! Couldn\'t find user for session:', sessionId);
       // Giving the wheel back to our bot
