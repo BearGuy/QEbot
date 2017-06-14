f77f36e4bc8a49573715bda36e36699671781b74
Bearguy <stephen@peterkins.ca>
2017-06-13 21:40:58 -0400

    Switched up the tokens

--- helper/send-helper.js
+++ helper/send-helper.js
@@ -286,7 +286,6 @@ const sender = {
         ]
       };
 
-
     request({
       uri: "https://graph.facebook.com/v2.6/me/thread_settings",
       qs: { access_token: process.env.PAGE_ACCESS_TOKEN },
